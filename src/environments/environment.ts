cd1068426565c01845586d8889101cdf85e71d81
Alessander Neal L. Rubiato <alessanderrubiato@yahoo.com>
2018-01-19 01:16:35 +0800

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBvIN56XpwM0Xgq10UJj_2QR819X0CXNgw",
+    authDomain: "product-listing-65f1f.firebaseapp.com",
+    databaseURL: "https://product-listing-65f1f.firebaseio.com",
+    projectId: "product-listing-65f1f",
+    storageBucket: "product-listing-65f1f.appspot.com",
+    messagingSenderId: "193215430219"
+    }
+};
